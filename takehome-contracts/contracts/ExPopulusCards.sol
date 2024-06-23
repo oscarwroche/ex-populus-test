0065c30a0caf4f7342d005878e3f65599e30023d
Oscar Roche <oscar.w.roche@gmail.com>
2024-06-23 02:35:35 +0200

    :sparkles: features for 1/2

--- takehome-contracts/contracts/ExPopulusCards.sol
+++ takehome-contracts/contracts/ExPopulusCards.sol
@@ -1,18 +1,60 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
 contract ExPopulusCards {
+    struct Card {
+        uint256 id;
+        uint256 health;
+        uint256 attack;
+        uint8 ability;
+    }
 
-	struct NftData {
-		uint8 attack;
-		uint8 health;
-		uint8 ability;
-	}
+    uint256 private nextCardId;
+    mapping(uint256 => Card) private cards;
+    mapping(uint8 => uint8) private abilityPriorities;
 
-	constructor() {
+    event CardMinted(uint256 indexed cardId, address indexed to, uint256 health, uint256 attack, uint8 ability);
+    event AbilityPriorityUpdated(uint8 indexed abilityId, uint8 priority);
 
-	}
+    constructor() {
+        // Initialize default ability priorities: Shield > Freeze > Roulette
+        abilityPriorities[0] = 0; // Shield
+        abilityPriorities[1] = 1; // Freeze
+        abilityPriorities[2] = 2; // Roulette
+    }
 
-	function mintCard() external {
+    function mintCard(address to, uint256 health, uint256 attack, uint8 ability) external returns (uint256) {
+        require(ability <= 2, "Ability value must be 0, 1, or 2");
 
-	}
+        uint256 cardId = nextCardId++;
+        cards[cardId] = Card({
+            id: cardId,
+            health: health,
+            attack: attack,
+            ability: ability
+        });
+
+        emit CardMinted(cardId, to, health, attack, ability);
+
+        return cardId;
+    }
+
+    function getCardDetails(uint256 cardId) external view returns (Card memory) {
+        require(cardId < nextCardId, "Card does not exist");
+        return cards[cardId];
+    }
+
+    function setAbilityPriority(uint8 abilityId, uint8 priority) external {
+        require(abilityId <= 2, "Ability id must be 0, 1, or 2");
+        for (uint8 i = 0; i <= 2; i++) {
+            require(abilityPriorities[i] != priority, "Priority already assigned to another ability");
+        }
+        abilityPriorities[abilityId] = priority;
+        emit AbilityPriorityUpdated(abilityId, priority);
+    }
+
+    function getAbilityPriority(uint8 abilityId) external view returns (uint8) {
+        require(abilityId <= 2, "Ability id must be 0, 1, or 2");
+        return abilityPriorities[abilityId];
+    }
 }
--- takehome-contracts/contracts/ExPopulusToken.sol
+++ takehome-contracts/contracts/ExPopulusToken.sol
@@ -1,12 +1,46 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
+import "./ExPopulusCards.sol";
+
 contract ExPopulusToken {
+    ExPopulusCards public cardsContract;
+    address public owner;
+    mapping(address => bool) private approvedMinters;
 
-	constructor() {
+    event MinterApproved(address indexed minter);
+    event MinterRevoked(address indexed minter);
 
-	}
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can perform this action");
+        _;
+    }
 
-	function mintToken() external {
+    modifier onlyOwnerOrApproved() {
+        require(msg.sender == owner || approvedMinters[msg.sender], "Not authorized to mint");
+        _;
+    }
 
-	}
+    constructor(address _cardsContractAddress) {
+        cardsContract = ExPopulusCards(_cardsContractAddress);
+        owner = msg.sender;
+    }
+
+    function approveMinter(address minter) external onlyOwner {
+        approvedMinters[minter] = true;
+        emit MinterApproved(minter);
+    }
+
+    function revokeMinter(address minter) external onlyOwner {
+        approvedMinters[minter] = false;
+        emit MinterRevoked(minter);
+    }
+
+    function mintToken(address to, uint256 health, uint256 attack, uint8 ability) external onlyOwnerOrApproved {
+        cardsContract.mintCard(to, health, attack, ability);
+    }
+
+    function setAbilityPriority(uint8 abilityId, uint8 priority) external onlyOwnerOrApproved {
+        cardsContract.setAbilityPriority(abilityId, priority);
+    }
 }
