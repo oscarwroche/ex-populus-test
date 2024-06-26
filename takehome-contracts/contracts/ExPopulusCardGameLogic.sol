5aa5043abb6b4abb4feec06ebf440db364192c1e
Oscar Roche <oscar.w.roche@gmail.com>
2024-06-26 02:52:53 +0200

    :fire: delete unused contract

--- takehome-contracts/contracts/ExPopulusCardGameLogic.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-import "hardhat/console.sol";
-import "./ExPopulusCards.sol";
-import "./ExPopulusToken.sol";
-
-contract ExPopulusCardGameLogic {}
